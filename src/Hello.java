d6385bb386bd66cd62119b807af75d6f1ee4fd1e
Harrison Nguyen <nguy2173@umn.edu>
2020-04-09 17:36:42 -0500

    declaring data type variable in java

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Harrison");
+
+        int myFirstNumber = 5;
+
+        System.out.println(myFirstNumber);
     }
 }
